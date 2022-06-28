e7bdf57dc29ebae2ff1c629420bcf78c649f89c1
Édison Renato Silva <edisonrenato@gmail.com>
2022-06-28 13:43:59 -0300

    JS Classes

---
+++ 040. JS/010. JS Objects/08.js
@@ -0,0 +1,5 @@
+let pen = {
+  brand: pigma,
+  tip: 0.4,
+  color:black
+}
---
+++ 040. JS/010. JS Objects/11.js
@@ -0,0 +1,12 @@
+const driver = {
+  name: 'Danica',
+  displayName: function () {
+    console.log(`Name: ${this.name}`);
+  }
+};
+
+const car = {
+  name: 'Fusion'
+};
+
+driver.displayName.bind(car);

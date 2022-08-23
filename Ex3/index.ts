de6f5082710428e07e01c5e5b128fd35e36f213b
Édison Renato Silva <edisonrenato@gmail.com>
2022-08-23 08:41:07 -0300

    TS working

---
+++ Ex3/index.ts
@@ -0,0 +1,5 @@
+const myName = "Edison";
+
+const hello = (userName: string): string => `hello, ${userName}`;
+
+console.log(hello(myName));
---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var myName = "Piroleibe";
+var hello = function (userName) { return "hello, ".concat(userName); };
+console.log(hello(myName));
